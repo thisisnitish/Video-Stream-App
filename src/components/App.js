7a5891d57a0f79517f6e809532badfbe288f7a3d
Nitish Kumar <nitishkumargenius@gmail.com>
2020-06-30 17:39:58 +0530

    Section 16 half done

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+    return <div>Page 1</div>;
+};
+
+const PageTwo = () => {
+    return(
+         <div>
+            Page 2
+            <button>Click Me</button>
+        </div>
+    );
+};
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+            <div>
+                <Route path="/" exact component={PageOne} />
+                <Route path="/pagetwo" component={PageTwo} />
+            </div>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
