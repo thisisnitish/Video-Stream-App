35be77a33f8ce94e2a9b89cb2fcbeb25921f478c
Nitish Kumar <nitishkumargenius@gmail.com>
2020-07-01 23:40:36 +0530

    Added streams folder for stream components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
-    return <div>Page 1</div>;
+    return (
+        <div>
+            Page 1
+            <Link to="/pagetwo">Navigate to Page 2</Link>
+        </div>
+    );
 };
 
+//Link is almost like the href but it does not load the page again and again
 const PageTwo = () => {
     return(
          <div>
             Page 2
             <button>Click Me</button>
+            <Link to="/">Navigate to Page 2</Link>
         </div>
     );
 };
