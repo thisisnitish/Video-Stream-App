76eef8c981f2de71420ce02df32fd3af805f83ae
Nitish Kumar <nitishkumargenius@gmail.com>
2020-07-06 00:04:31 +0530

    Integrated react redux

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -24,11 +24,11 @@ class GoogleAuth extends React.Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     };
 
-    onSignIn = () => {
+    onSignInClick = () => {
         this.auth.signIn();
     };
 
-    onSignOut = () => {
+    onSignOutClick = () => {
         this.auth.signOut();
     };
 
@@ -38,7 +38,7 @@ class GoogleAuth extends React.Component {
         }
         else if(this.state.isSignedIn) {
             return (
-                <button onClick={this.onSignOut} className="ui red google button">
+                <button onClick={this.onSignOutClick} className="ui red google button">
                     <i className="google icon"/>
                     Sign Out
                 </button>
@@ -46,7 +46,7 @@ class GoogleAuth extends React.Component {
         }
         else{
             return (
-                <button onClick={this.onSignIn} className="ui red google button">
+                <button onClick={this.onSignInClick} className="ui red google button">
                     <i className="google icon"/>
                     Sign In with Google
                 </button>
