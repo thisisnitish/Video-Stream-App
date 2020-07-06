6c096f18416e1e32b7277166319a051352223a38
Nitish Kumar <nitishkumargenius@gmail.com>
2020-07-06 23:46:38 +0530

    Implemented form validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,11 +3,12 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
 
-    renderInput({ input, label }) {
+    renderInput({ input, label, meta }) {
         return (
             <div className="field">
                 <label>{label}</label>
                 <input {...input} />
+                <div>{meta.error}</div>
             </div>
         );
     }
@@ -25,8 +26,22 @@ class StreamCreate extends React.Component {
         </form>
         );
     }
+}
+
+const validate = (formValues) => {
+    const errors = {};
+    if(!formValues.title){
+        errors.title = "You must enter a title";
+    }
+
+    if(!formValues.description){
+        errors.description = "You must enter a description";
+    }
+
+    return errors;
 };
 
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate: validate
 })(StreamCreate);
